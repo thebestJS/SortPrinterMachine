c5b4b037c630996ba2af32ee66d9709f86849d6f
thebestJS <58833077+thebestJS@users.noreply.github.com>
2024-03-13 11:47:19 +0900

    Update SortSelection.java

--- src/SortSelection.java
+++ src/SortSelection.java
@@ -1,2 +1,16 @@
-public class SortSelection {
+import java.util.List;
+
+public class SortSelection implements SortTest {
+    @Override
+    public void sort(List<Integer> itemList) {
+        for(int num_i = 0 ; num_i < itemList.size(); num_i++) {
+            for(int num_j = num_i + 1 ; num_j < itemList.size(); num_j ++) {
+                if(itemList.get(num_i) > itemList.get(num_j)){
+                    int tmp = itemList.get(num_i);
+                    itemList.set(num_i, itemList.get(num_j));
+                    itemList.set(num_j, tmp);
+                }
+            }
+        }
+    }
 }
