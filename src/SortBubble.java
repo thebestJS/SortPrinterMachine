3c44dbd8726fc2ff29b00a02ab6fe89a57df7d75
thebestJS <58833077+thebestJS@users.noreply.github.com>
2024-03-13 13:08:18 +0900

    Update SortBubble.java

--- src/SortBubble.java
+++ src/SortBubble.java
@@ -1,48 +1,17 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class SortPrinter {
-    private SortTest sortTest;
-    private final List<Integer> itemList = new ArrayList<>();
-
-    public void insert(int item) {
-        itemList.add(item);
-    }
-
-    public void selection(SortTest sTest) {
-        this.sortTest = sTest;
-    }
-
-    public void run() {
-        sortTest.sort(itemList);
-    }
-
-    public void show() {
-        System.out.println(sortTest.getClass().getSimpleName());
-        System.out.println(Arrays.toString(itemList.toArray()));
-        System.out.println();
-    }
-
-
-    public static void main(String[] args) {
-        SortPrinter sortPrinter = new SortPrinter();
-        sortPrinter.insert(5);
-        sortPrinter.insert(4);
-        sortPrinter.insert(3);
-        sortPrinter.insert(2);
-        sortPrinter.insert(1);
-        sortPrinter.insert(6);
-        sortPrinter.insert(9);
-        sortPrinter.insert(8);
-        sortPrinter.insert(7);
-
-        sortPrinter.selection(new SortBubble());
-        sortPrinter.run();
-        sortPrinter.show();
-
-        sortPrinter.selection(new SortSelection());
-        sortPrinter.run();
-        sortPrinter.show();
+public class SortBubble implements SortTest {
+    @Override
+    public void sort(List<Integer> itemList) {
+        for(int cycle = 0 ;cycle < itemList.size(); cycle ++) {
+            for(int num_i = 0 ; num_i < itemList.size() - 1 - cycle; num_i ++) {
+                if(itemList.get(num_i) > itemList.get(num_i + 1)){
+                    //swap
+                    int tmp = itemList.get(num_i);
+                    itemList.set(num_i, itemList.get(num_i + 1));
+                    itemList.set(num_i + 1, tmp);
+                }
+            }
+        }
     }
 }
