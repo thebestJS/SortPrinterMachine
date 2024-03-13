117d2fa87218b14c349c2001d799afb08de7ce77
thebestJS <58833077+thebestJS@users.noreply.github.com>
2024-03-13 11:46:53 +0900

    Update SortBubble.java

--- src/SortBubble.java
+++ src/SortBubble.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class SortBubble {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SortPrinter {
+    private SortTest sortTest;
+    private final List<Integer> itemList = new ArrayList<>();
+
+    public void insert(int item) {
+        itemList.add(item);
+    }
+
+    public void selection(SortTest sTest) {
+        this.sortTest = sTest;
+    }
+
+    public void run() {
+        sortTest.sort(itemList);
+    }
+
+    public void show() {
+        System.out.println(sortTest.getClass().getSimpleName());
+        System.out.println(Arrays.toString(itemList.toArray()));
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        SortPrinter sortPrinter = new SortPrinter();
+        sortPrinter.insert(5);
+        sortPrinter.insert(4);
+        sortPrinter.insert(3);
+        sortPrinter.insert(2);
+        sortPrinter.insert(1);
+        sortPrinter.insert(6);
+        sortPrinter.insert(9);
+        sortPrinter.insert(8);
+        sortPrinter.insert(7);
+
+        sortPrinter.selection(new SortBubble());
+        sortPrinter.run();
+        sortPrinter.show();
+
+        sortPrinter.selection(new SortSelection());
+        sortPrinter.run();
+        sortPrinter.show();
+    }
 }
